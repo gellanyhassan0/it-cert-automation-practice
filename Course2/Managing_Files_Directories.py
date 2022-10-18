887339fefc08ee148db35ea7e77fe707794c3daf
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-10-18 19:33:55 +0000

    Update Managing_Files_Directories.py

--- Course2/Managing_Files_Directories.py
+++ Course2/Managing_Files_Directories.py
@@ -9,5 +9,27 @@ def create_python_script(filename):
 
 print(create_python_script("program.py"))
 
+import os
+
+def new_directory(directory, filename):
+  # Before creating a new directory, check to see if it already exists
+  if os.path.isdir(directory) == False:
+          
+          parent_dir = os.path.abspath(os.getcwd())
+          path = os.path.join(parent_dir, directory)
+          os.mkdir(path)
+          print(os.getcwd())
+          print(os.listdir(os.getcwd()))
+
+  # Create the new file inside of the new directory
+  os.chdir(directory)
+  with open(filename, "w") as file:
+       file.close()
+
+  # Return the list of files in the new directory
+  return (os.listdir(os.getcwd()))
+
+print(new_directory("PythonPrograms", "script.py"))
+
 
 
